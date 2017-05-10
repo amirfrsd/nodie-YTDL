12f6fd1001410fc40f4fcd6e2f43855093524f99
Amir Hossein <amir@Amirs-iMac-2.local>
2017-05-11 01:20:49 +0430

    The big thing is here, JK, Simple Insensitive Filter is included

--- index.js
+++ index.js
@@ -7,26 +7,43 @@ var router = express.Router();
 var fs = require('fs');
 var youtubeDl = require('youtube-dl');
 var ytdl = require('ytdl-core');
+var badWords = ["sex","porn","sexy","nude","pornstar","xxx","سکس","پورن","جنده","کس","کیر","کون","dick"];
+var badWordNegatives = ["movie","song","clip","album","track","music","book","magazine","آهنگ","فیلم","کلیپ"];
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use('/',router);
 var isValidSearchTerm = function(searchQuery) {
+    var query = searchQuery+""
+    if (new RegExp(badWords.join("|")).test(query)) {
+        if (new RegExp(badWordNegatives.join("|")).test(query)) {
+            return true;
+        } else{ 
+            return false;
+        }
+    } else {
+        return true;
+    }
 }
+console.log(isValidSearchTerm("sexy hot girl"));
 router.use(function(req,res,next){
     next();
 });
 router.post('/search',function(req,res){
     var opts = {maxResults:20,key:'AIzaSyBsTzQhNubTEN-xwzSTZRwUEUSbZYEcRss',type:'video'};
-    youtubeSearch(req.body.query,opts,function(err,results){
-        if(err) return console.log(err);
-        res.json({success:true,results});
-    });
+    var queryToCheck = req.body.query;
+    if (isValidSearchTerm(queryToCheck)) {
+        youtubeSearch(req.body.query,opts,function(err,results){
+            if(err) return console.log(err);
+            res.json({success:true,results});
+        });
+    } else {
+        res.json({success:false,messsage:"جستجوی شما نتیجه ای نداد"})
+    }
 });
 router.post('/download',function(req,res){
     var id = req.body.link.substr(req.body.link.length - 11);
     ytdl(req.body.link,{ filter: function(format) { return format.container === 'mp4'; }}).pipe(fs.createWriteStream('../static_content/' + id + '.mp4'))
         res.json({success:true, message:'https://myfuckingapi.com/'+id+'.mp4'});
-  
 });
 router.post('/finish',function(req,res){
     var id = req.body.link.substr(req.body.link.length - 11);
